15c289ec23e175ad498388142425bf91f7fc00be
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-15 11:29:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,8 @@ using namespace geode::prelude;
 class $modify(GameManager) {
     void setGameVariable(const std::string& key, bool value) {
         if (key == "v_sync") {
-            // force disable vsync or TPS cap toggle
-            value = false;
+            value = false; // Force disable VSync (or any other game var override)
         }
-        GameManager::setGameVariable(key, value);
+        GameManager::setGameVariable(key.c_str(), value);
     }
 };
