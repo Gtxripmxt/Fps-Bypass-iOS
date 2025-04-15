5d4af5d8c75d278ac7d4eb16182ee409cfe069d3
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-15 12:12:10 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace geode::prelude;
 class $modify(MyScheduler, cocos2d::CCScheduler) {
 public:
     void update(float dt) override {
-        dt = 1.0f / 240.0f; // 240 TPS or whatever i will make this sdgjfgfdfdgfdgdfgsdfjgfdkgdfkhgdfkghsfdlkhgfdklgfdkljgldkfsdlkj same as the number in the settings
+        dt = 1.0f / 1440.0f; // any tps or whatever i will make this sdgjfgfdfdgfdgdfgsdfjgfdkgdfkhgdfkghsfdlkhgfdklgfdkljgldkfsdlkj same as the number in the settings
         log::info("FPS bypass active - forcing dt = {}", dt);
         CCScheduler::update(dt);
     }
