4cace4f321831d972e837fbe7bb7c6493365a95d
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-15 11:22:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
-//hmmmm
+#include <Geode/Geode.hpp>
+#include <Geode/modify/CCScheduler.hpp>
+
+using namespace geode::prelude;
+
+class $modify(CCScheduler) {
+public:
+    void tick(float dt) {
+        dt = 1.0f / 240.0f; // Set to 240 FPS wooooo
+        return $orig(dt);
+    }
+}:
